ce57310aae379fa264d163f590e8f3a18e75692d
Joe Gakumo <joegakumo1@gmail.com>
2021-08-29 17:20:40 +0300

    add variable to get data and store them from git

--- Github-Search/src/app/components/users/users.component.ts
+++ Github-Search/src/app/components/users/users.component.ts
@@ -9,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class UsersComponent implements OnInit {
    usersDetails:any  ;
    repository:any;
+   searchUser:any;
 
   constructor( private userservice:UsersService) { 
     this.userservice.getUsersDetails().subscribe((usersDetails:any[])=>{
